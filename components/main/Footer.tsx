2cbc6d62f79fb5f711c0645080748181c3ce7d35
silmaraMoreira <silmaraa.1994@icloud.com>
2024-04-29 18:31:37 -0300

    adding new style

---
+++ components/main/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="w-full bg-transparent text-gray-200 shadow-lg p-[15px] text-center">
+      &copy; Corder Girl Dev 2024 Inc. All rights reserved
+    </div>
+  );
+};
+
+export default Footer;
---
+++ components/sub/SkillText.tsx
@@ -0,0 +1,19 @@
+"use client";
+import React from "react";
+import { motion } from "framer-motion";
+import { slideInFromLeft, slideInFromRight } from "@/utils/motion";
+
+const SkillText = () => {
+  return (
+    <div className="w-full h-auto flex flex-col items-center justify-center">
+      <motion.div
+        variants={slideInFromLeft(0.5)}
+        className="text-[30px] text-white font-medium mt-[10px] text-center mb-[15px]"
+      >
+        Tecnologias
+      </motion.div>
+    </div>
+  );
+};
+
+export default SkillText;
